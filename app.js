fe876cd231c63f4b19b82fca8ccb35946a142535
Sayan Biswas <bsayan289@gmail.com>
2022-05-20 22:42:01 +0530

    create security

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 // const Joi = require('joi');
 // const Review = require('./models/review');
+// const helmet = require('helmet');
 
 const session = require('express-session');
 
@@ -23,19 +24,25 @@ const methodOverride = require('method-override');
 // const Campground = require('./models/campground');
 // const {campgroundSchema, reviewSchema} =require('./schemas');
 
+const mongoSanitize = require('express-mongo-sanitize');
+
 const reviewRoutes = require('./routes/reviews');
 const campgroundRoutes = require('./routes/campground');
 const userRoutes = require('./routes/users');
 
 const User = require('./models/user');
 const passport = require('passport');
+
 const LocalStrategy = require('passport-local');
 
+const MongoDBStore = require('connect-mongo')(session);
 
 
-
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+// const dbUrl = 'mongodb://localhost:27017/yelp-camp';
 //Database
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+//
+mongoose.connect(dbUrl, {
     useNewUrlParser : true,
     // useCreateIndex: true,
     useUnifiedTopology : true,
@@ -59,14 +66,82 @@ app.use(express.urlencoded({ extended : true }));
 app.use(methodOverride('_method'));
 
 app.use(flash());
+// app.use(helmet({
+//     frameguard : false,
+// }))
+
+// const scriptSrcUrls = [
+//     "https://stackpath.bootstrapcdn.com/",
+//     "https://api.tiles.mapbox.com/",
+//     "https://api.mapbox.com/",
+//     "https://kit.fontawesome.com/",
+//     "https://cdnjs.cloudflare.com/",
+//     "https://cdn.jsdelivr.net",
+// ];
+// const styleSrcUrls = [
+//     "https://kit-free.fontawesome.com/",
+//     "https://stackpath.bootstrapcdn.com/",
+//     "https://api.mapbox.com/",
+//     "https://api.tiles.mapbox.com/",
+//     "https://fonts.googleapis.com/",
+//     "https://use.fontawesome.com/",
+// ];
+// const connectSrcUrls = [
+//     "https://api.mapbox.com/",
+//     "https://a.tiles.mapbox.com/",
+//     "https://b.tiles.mapbox.com/",
+//     "https://events.mapbox.com/",
+// ];
+// const fontSrcUrls = [];
+// app.use(
+//     helmet.contentSecurityPolicy({
+//         directives: {
+//             defaultSrc: [],
+//             connectSrc: ["'self'", ...connectSrcUrls],
+//             scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+//             styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+//             workerSrc: ["'self'", "blob:"],
+//             objectSrc: [],
+//             imgSrc: [
+//                 "'self'",
+//                 "blob:",
+//                 "data:",
+//                 "https://res.cloudinary.com/dfw3onnwx/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+//                 "https://images.unsplash.com/",
+//             ],
+//             fontSrc: ["'self'", ...fontSrcUrls],
+//         },
+//     })
+// );
+
+const secret = process.env.SECRET || 'thisshouldbeabettersecr';
+
+app.use(mongoSanitize({
+    replaceWith :'_'
+}));
+
+const store = new MongoDBStore({
+    url: dbUrl,
+    secret,
+    touchAfter : 24 * 60 * 60,
+
+});
+
+store.on('error', function(e){
+    console.log('store error', e);
+})
+
 const sessionConfig = {
-    secret : 'thisshouldbeabettersecr',
+    store,
+    name : 'session',
+    secret,
     resave : false,
     saveUninitialized : true,
     // store : assd
     cookie : {
 
         httpOnly : true,
+        // secure : true,
         expires : Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge : 1000 * 60 * 60 * 24 * 7
 
@@ -127,8 +202,9 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
 
-    console.log(req.session);
+    // console.log(req.session);
 
+    console.log(req.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
